cd00f7cccb9e9c2a202f4b20764f8aff2c4edcea
Sreeram Vipparla <86887626+SreeramVipparla@users.noreply.github.com>
2021-12-25 07:33:34 +0400

    Update ImageService.java

--- catpoint-parent/image/src/main/java/com/udacity/catpoint/image/service/ImageService.java
+++ catpoint-parent/image/src/main/java/com/udacity/catpoint/image/service/ImageService.java
@@ -2,6 +2,7 @@ package com.udacity.catpoint.image.service;
 
 import java.awt.image.BufferedImage;
 
+
 public interface ImageService {
     boolean imageContainsCat(BufferedImage image, float confidenceThreshhold);
 }
